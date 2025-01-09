3bb222243e6a7e181b786927e6db1f49ade180b7
Brayan Munoz Campos <brayan.munoz.2306@gmail.com>
2025-01-08 23:38:01 -0500

    first commit

---
+++ ProductsAppUITests/ProductsAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ProductsAppUITestsLaunchTests.swift
+//  ProductsAppUITests
+//
+//  Created by Brayan Munoz on 8/01/25.
+//
+
+import XCTest
+
+final class ProductsAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# productsapp-ios
+
+## Build and run
+Open your terminal, then:
+
+1. Checkout source code
+   ```
+   git clone https://github.com/BrayanMC/productsapp-ios
+   cd productsapp-ios
+   ```
+
+2. Install dependencies
+   ```
+   pod install
+   ```
+
+3. Open `Products.xcworkspace` with Xcode, hit `⌘R` to run.
